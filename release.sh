76f6def28b4850abc7afd8144d73d184658cc1ab
Shalin <shalinvs@gmail.com>
2021-11-11 12:20:50 -0800

    Incrementing version number to v0.2.4

--- release.sh
+++ release.sh
@@ -28,16 +28,6 @@ releaseBranch=release-$versionLabel
 # create the release branch from the -develop branch
 git checkout -b $releaseBranch
  
-# file in which to update version number
-versionFile="version.txt"
- 
-# find version number assignment ("= v1.5.5" for example)
-# and replace it with newly specified version number
-sed -i.backup -E "s/\= v[0-9.]+/\= $versionLabel/" $versionFile $versionFile
- 
-# remove backup file created by sed command
-rm $versionFile.backup
- 
 # commit version number increment
 git commit -am "Incrementing version number to $versionLabel"
  
